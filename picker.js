fb9e5fe61479c314dcd6fcd05ef2aac58aba8162
Luciano de Souza <luci0871@stud.kea.dk>
2020-02-19 13:38:11 +0100

    testing branch

--- picker.js
+++ picker.js
@@ -1,34 +1,32 @@
 // Simple example, see optional options for more configuration.
 const pickr = Pickr.create({
-    el: '.color-picker',
-    theme: 'classic', // or 'monolith', or 'nano'
+  el: ".color-picker",
+  theme: "classic", // or 'monolith', or 'nano'
 
-    components: {
+  components: {
+    // Main components
+    preview: true,
+    opacity: true,
+    hue: true,
 
-        // Main components
-        preview: true,
-        opacity: true,
-        hue: true,
-
-        // Input / output Options
-        interaction: {
-            hex: true,
-            rgba: true,
-            hsla: true,
-            hsva: false,
-            cmyk: false,
-            input: true,
-            clear: true,
-            save: true
-        }
+    // Input / output Options
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      hsva: false,
+      cmyk: false,
+      input: true,
+      clear: true,
+      save: true
     }
+  }
 });
 
-pickr.on('change', (...args) => {
-    let color = args[0].toRGBA();
-    console.log(color);
-    this.panel.style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`;
-
+pickr.on("change", (...args) => {
+  let color = args[0].toRGBA();
+  console.log(color);
+  this.panel.style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`;
 });
 
 let panel = document.getElementById("panel");
@@ -43,28 +41,25 @@ const min = Math.min(r, g, b);
 const max = Math.max(r, g, b);
 
 if (max === min) {
-    h = 0;
-} else
-if (max === r) {
-    h = 60 * (0 + (g - b) / (max - min));
-} else
-if (max === g) {
-    h = 60 * (2 + (b - r) / (max - min));
-} else
-if (max === b) {
-    h = 60 * (4 + (r - g) / (max - min));
+  h = 0;
+} else if (max === r) {
+  h = 60 * (0 + (g - b) / (max - min));
+} else if (max === g) {
+  h = 60 * (2 + (b - r) / (max - min));
+} else if (max === b) {
+  h = 60 * (4 + (r - g) / (max - min));
 }
 
 if (h < 0) {
-    h = h + 360;
+  h = h + 360;
 }
 
 l = (min + max) / 2;
 
 if (max === 0 || min === 1) {
-    s = 0;
+  s = 0;
 } else {
-    s = (max - l) / (Math.min(l, 1 - l));
+  s = (max - l) / Math.min(l, 1 - l);
 }
 // multiply s and l by 100 to get the value in percent, rather than [0,1]
 s *= 100;
@@ -91,6 +86,3 @@ console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
 }).on('swatchselect', (color, instance) => {
     console.log('swatchselect', color, instance);
 }); */
-
-
-line to test
